b1f2a0f186f50d9071211715f75176f9789ac377
siska <doter2016@ukr.net>
2022-10-18 00:53:14 +0300

    initial commit

---
+++ testwork.js
@@ -0,0 +1,50 @@
+const http = require('http');
+const url = require('url');
+const request = require('request');
+const res = require("express/lib/response");
+
+let port = 3000;
+
+function coinCapCurrency(req, res) {
+
+    let url_parts = url.parse(req.url, true);
+
+    if (url_parts.pathname === '/rates') {
+        let currency = url_parts.query['currency'];
+
+        request({
+            followAllRedirect: true,
+            url: 'https://api.coincap.io/v2/rates' + '/' + currency
+        }, function (error, responce, body) {
+            if (!error) {
+                try {
+                    let parsedJSON = JSON.parse(body)
+                    res.writeHead(200, {'Content-type': 'application/json'});
+                    res.write(JSON.stringify({
+                        usd: parsedJSON['data']['rateUsd']
+                    }))
+                } catch (err) {
+                    console.error(err);
+                    returnError(res);
+                } finally {
+                    res.end()
+                }
+            } else {
+                console.log(error);
+                returnError(res);
+                res.end()
+            }
+
+        })
+    }
+}
+
+function returnError(response) {
+    response.writeHead(404, {'Content-type': 'application/json'});
+    response.write(JSON.stringify({
+        error: 'invalid request'
+    }))
+}
+
+let server = http.createServer(coinCapCurrency);
+server.listen(port, 'localhost')
